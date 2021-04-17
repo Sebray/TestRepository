241b114528f03600ad74764a9524121deee234af
Sebray <and.swiridow2014@yandex.ru>
2021-04-17 13:07:31 +0300

    Added pop-up

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-debugger;
+
 table.onclick = function(event){
     let target = event.target.closest(".b_show");
+    if(target != null){
+       let parent = upTo(target, 'tr');
+
+       let strOfRecord = parent.cells[0].innerText;
+       for(let i = 1; i < parent.cells.length - 1; i++){
+           strOfRecord = strOfRecord.concat(', ' +  parent.cells[i].innerText);
+       }
+       let popup = document.createElement('div');
+       popup.className = 'b-popup';
+       let content = document.createElement('div');
+       content.className = 'popup-content'
+       let text = document.createElement('p');
+       text.innerHTML = strOfRecord;
+       let button_close = document.createElement('button');
+
+       button_close.innerHTML = 'закрыть';
+       button_close.id = 'b_close';
+       button_close.addEventListener('click', {
+        handleEvent(event) {
+            upToUsingTagClass(event, 'b-popup').remove();
+        }
+      });
+
+       content.appendChild(text);
+       content.appendChild(button_close);
+       
+       popup.appendChild(content);
+       document.getElementById('table').insertAdjacentElement('afterend', popup);
+    }
 }
-elem.onclick = function(event) {
-    // вывести тип события, элемент и координаты клика
-    alert(event.type + " на " + event.currentTarget);
-    alert("Координаты: " + event.clientX + ":" + event.clientY);
-  };
- let y = document.getElementsByClassName("b_show")
-document.getElementsByClassName("b_show")[0].onclick = function(event) {
-    // вывести тип события, элемент и координаты клика
-    alert(event.type + " на " + event.currentTarget);
-    alert("Координаты: " + event.clientX + ":" + event.clientY);}
+
+function upToUsingTagClass(el, tagClass) {
+    el = el.target.closest(".popup-content");
+    while (el) {
+      el = el.parentNode;
+      if (el.className && el.className == tagClass) {
+        return el;
+      }
+    }
+   return null;
+  }
+
+function upTo(el, tagName) {
+    tagName = tagName.toLowerCase();
+  
+    while (el && el.parentNode) {
+      el = el.parentNode;
+      if (el.tagName && el.tagName.toLowerCase() == tagName) {
+        return el;
+      }
+    }
+   return null;
+  }
